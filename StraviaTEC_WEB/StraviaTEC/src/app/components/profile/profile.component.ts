ef8be305946f0d6a2faf11c663904b291734a3ae
MAU <mau141226@gmail.com>
2022-05-25 20:17:48 -0600

    [Update]

--- StraviaTEC_WEB/StraviaTEC/src/app/components/profile/profile.component.ts
+++ StraviaTEC_WEB/StraviaTEC/src/app/components/profile/profile.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  profiledata = [{
+    "name":"Ariana",
+    "last_name": "Solano Rodriguez",
+    "birthdate":"12-06-1998",
+    "nationality":"Canadian",
+    "age":"24",
+    "followers": "243",
+    "following":"87",
+    "activities":"155",
+    "image":"https://m.media-amazon.com/images/I/31LtVzDD8AL._SL500_.jpg",
+
+  },]
+
+  activitydata = [{
+    "name" : "Ariana Solano",
+    "no_activity":"72347634",
+    "type": "Cycling",
+    "time":"1:38 PM",
+    "date":"24-05-2022",
+    "duration":"54:00",
+    "distance": "54 Km",
+    "gpx":"src\assets\gpx\1.gpx",
+  },]
+
+  latitude = 51.678418;
+  longitude = 7.809007;
+
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
