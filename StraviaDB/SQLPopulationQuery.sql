bc08e47594d1791c55ab0a14c88432a63383a624
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2022-05-31 23:04:12 -0600

    [Update] API Connection

---
+++ StraviaDB/SQLPopulationQuery.sql
@@ -0,0 +1,58 @@
+USE [StraviaDB]
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Junior'
+           ,'menor de 15 años')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Sub-23'
+           ,'de 15 a 23 años')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Open'
+           ,'de 24 a 30 años')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Elite'
+           ,'cualquiera que quiera inscribirse')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Master A'
+           ,'de 30 a 40 años')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Master B'
+           ,'de 41 a 50 años')
+GO
+
+INSERT INTO [dbo].[Category]
+           ([category]
+           ,[description])
+     VALUES
+           ('Master C'
+           ,'más de 51 años')
+GO
