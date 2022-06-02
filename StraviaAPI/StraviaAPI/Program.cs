8a797a0c0460a9c269dae4ff07875d03f24a412b
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2022-06-02 11:20:11 -0600

    [Fix] API

--- StraviaAPI/StraviaAPI/Program.cs
+++ StraviaAPI/StraviaAPI/Program.cs
@@ -15,10 +15,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+{ }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(x => x
             .AllowAnyOrigin()
