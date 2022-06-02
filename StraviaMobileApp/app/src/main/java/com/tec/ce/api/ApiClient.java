d0409f925c9f3713825950304c1957c4a2ad3924
Yendry <yen.badilla@gmail.com>
2022-06-02 00:04:06 -0600

    [Add] App Connection to API 90%

--- StraviaMobileApp/app/src/main/java/com/tec/ce/api/ApiClient.java
+++ StraviaMobileApp/app/src/main/java/com/tec/ce/api/ApiClient.java
@@ -2,15 +2,22 @@ package com.tec.ce.api;
 
 import com.tec.ce.api.services.UserService;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
     private static Retrofit getRetrofit(){
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://straviaapi.azurewebsites.net/")
+                .baseUrl("https://straviaapi.azurewebsites.net/User")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
         return retrofit;
     }
