4521028f7a03fc970e13800e3a6fce4fd5484264
MAU <mau141226@gmail.com>
2022-06-03 00:06:40 -0600

    [Add] new model and interfaces created

---
+++ StraviaTEC_WEB/StraviaTEC/src/app/model/register.ts
@@ -0,0 +1,10 @@
+export class Register {
+  name:string = ''
+  lastname:string= ''
+  nationality: string= ''
+  birthdate:string= ''
+  category:string= ''
+  username:string= ''
+  password:string= ''
+  image:string= ''
+}
