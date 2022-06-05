de7ee0f7e3f2c8600d25b0dd64864445d73e0791
MAU <mau141226@gmail.com>
2022-06-05 01:20:56 -0600

    [Update] Bug fixed

--- StraviaTEC_WEB/StraviaTEC/src/app/model/create-race.ts
+++ StraviaTEC_WEB/StraviaTEC/src/app/model/create-race.ts
@@ -1,17 +1,15 @@
-import jsPDF from "jspdf"
-
 export class CreateRace {
-  username: string = ''
-  name: string = ''
-  date:string = ''
-  time:string= ''
-  type: string = ''
-  distance:number= 0
-  altitude:number= 0
-  price:number= 0
-  privacity:string = ''
-  route:string = ''
-  categories:Array<any> = []
-  sponsors:Array<any> = []
-  bank_accounts:Array<any> = []
+  username: string = '';
+  name: string = '';
+  date: string = '';
+  time: string = '';
+  type: string = '';
+  distance: number = 0;
+  altitude: number = 0;
+  price: number = 0;
+  privacity: string = '';
+  route: string = '';
+  categories: Array<any> = [];
+  sponsors: Array<any> = [];
+  bank_accounts: Array<any> = [];
 }
